c69c65dc9da85d8444096ba91233ae9ca65f8ccf
minui2167 <minui2167@gmail.com>
2022-08-09 16:39:01 +0900

    테스트

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-# 안녕하세요.
+from flask import Flask
+from flask_restful import Api
+
+app = Flask(__name__)
+
+api = Api(app)
+
+@app.route('/')
+def index():
+    return 'Hello, Flask!'
+
+if __name__ == '__main__' :
+    app.run()
+
+
+# 테스트 
