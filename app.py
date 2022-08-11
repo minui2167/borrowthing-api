50d68279111319a2e3fd1a5f12aba053acb77a77
ghdrl858 <ghdrl858@gmail.com>
2022-08-11 17:49:46 +0900

    회원가입, 로그인, 로그아웃, 내 정보 가져오기

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
 from flask import Flask
 from flask_restful import Api
+from config import Config
+from flask_jwt_extended import JWTManager
+
+from resources.users import UserEditResource, UserLoginResource, UserLogoutResource, UserRegisterResource, jwt_blacklist
 
 app = Flask(__name__)
 
+# 환경변수 셋팅
+app.config.from_object(Config)
+
+# JWT 토큰 라이브러리 만들기
+jwt = JWTManager(app)
+
+# 로그아웃 된 토큰이 들어있는 set을, jwt 에 알려준다.
+@jwt.token_in_blocklist_loader
+def check_if_token_is_revoked(jwt_header, jwt_payload):
+    jti = jwt_payload['jti']
+    return jti in jwt_blacklist
+
 api = Api(app)
 
-@app.route('/')
-def index():
-    return 'Hello, Flask!'
+# 경로와 리소스(API 코드)를 연결한다.
+api.add_resource(UserRegisterResource, '/users/register')
+api.add_resource(UserLoginResource, '/users/login')
+api.add_resource(UserLogoutResource, '/users/logout')
+api.add_resource(UserEditResource, '/users/edit')
 
 if __name__ == '__main__' :
     app.run()
-
-
-# 테스트 
