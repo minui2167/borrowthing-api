80036b822554a5b1404091a8c29becd214f94b93
minui2167 <minui2167@gmail.com>
2022-08-12 13:34:18 +0900

    게시물 생성, 목록, 수정

--- resources/community.py
+++ resources/community.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from os import remove
 from flask_restful import Resource
 from flask import request
 from mysql_connection import get_connection
@@ -16,42 +17,8 @@ class PostingListResource(Resource) :
         # 1. 클라이언트로부터 데이터를 받아온다.
         userId = get_jwt_identity()
 
-        # photo(file), content(text)
-
-        if 'photo1' not in request.files:
-            return {'error' : '파일을 업로드하세요'}, 400
-
-        # 2. S3에 파일 업로드
-        # 클라이언트로부터 파일을 받아온다.
-        file = request.files['photo1']
         content = request.form['content']
 
-        # 파일명을 우리가 변경해 준다.
-        # 파일명은, 유니크하게 만들어야 한다.
-        current_time = datetime.now()
-        new_file_name = current_time.isoformat().replace(':', '_') + ('.jpg')
-
-        # 유저가 올린 파일의 이름을 내가 만든 파일명으로 변경
-        file.filename = new_file_name
-
-        # S3에 업로드 하면 된다.
-        # AWS의 라이브러리를 사용해야 한다.
-        # 이 파이썬 라이브러리가 boto3 라이브러리다.
-        # pip install boto3
-        s3 = boto3.client('s3', 
-                    aws_access_key_id = Config.ACCESS_KEY,
-                    aws_secret_access_key = Config.SECRET_ACCESS)
-
-        try :
-            s3.upload_fileobj(file,             # 업로드 파일
-                            Config.S3_BUCKET,   # 버킷 url
-                            file.filename,      # 파일명
-                            ExtraArgs = {'ACL' : 'public-read', 'ContentType' : file.content_type})    # 권한, 타입
-
-        except Exception as e:
-            return {'error' : str(e)}, 500
-
-        
         # 게시물 작성
         # 3. DB에 저장
         try :
@@ -89,89 +56,361 @@ class PostingListResource(Resource) :
             cursor.close()
             connection.close()
             return {"error" : str(e)}, 503
-        
-        # 사진을 DB에 저장
-        try :
-            # 데이터 insert
-            # 1. DB에 연결
-            connection = get_connection()
             
-            # 2. 쿼리문 만들기
-            query = '''insert into images
-                    (userId, imageUrl)
-                    values
-                    (%s, %s);'''
+
+        # photo(file), content(text)
+        photoList = ['photo1', 'photo2', 'photo3']
+        for photo in photoList :
+            if photo in request.files:
+                # 2. S3에 파일 업로드
+                # 클라이언트로부터 파일을 받아온다.
+                file = request.files[photo]
+
+                # 파일명을 우리가 변경해 준다.
+                # 파일명은, 유니크하게 만들어야 한다.
+                current_time = datetime.now()
+                new_file_name = current_time.isoformat().replace(':', '_') + ('.jpg')
+
+                # 유저가 올린 파일의 이름을 내가 만든 파일명으로 변경
+                file.filename = new_file_name
+                print(file.filename)
+                # S3에 업로드 하면 된다.
+                # AWS의 라이브러리를 사용해야 한다.
+                # 이 파이썬 라이브러리가 boto3 라이브러리다.
+                # pip install boto3
+                s3 = boto3.client('s3', 
+                            aws_access_key_id = Config.ACCESS_KEY,
+                            aws_secret_access_key = Config.SECRET_ACCESS)
+
+                try :
+                    s3.upload_fileobj(file,             # 업로드 파일
+                                    Config.S3_BUCKET,   # 버킷 url
+                                    file.filename,      # 파일명
+                                    ExtraArgs = {'ACL' : 'public-read', 'ContentType' : file.content_type})    # 권한, 타입
+
+                except Exception as e:
+                    return {'error' : str(e)}, 500       
+
+                # 사진을 DB에 저장
+                try :
+                    # 데이터 insert
+                    # 1. DB에 연결
+                    connection = get_connection()
                     
-            # recode 는 튜플 형태로 만든다.
-            recode = (userId, file.filename)
+                    # 2. 쿼리문 만들기
+                    query = '''insert into images
+                            (userId, imageUrl)
+                            values
+                            (%s, %s);'''
+                            
+                    # recode 는 튜플 형태로 만든다.
+                    recode = (userId, file.filename)
 
-            # 3. 커서를 가져온다.
-            cursor = connection.cursor()
+                    # 3. 커서를 가져온다.
+                    cursor = connection.cursor()
 
-            # 4. 쿼리문을 커서를 이용해서 실행한다.
-            cursor.execute(query, recode)
+                    # 4. 쿼리문을 커서를 이용해서 실행한다.
+                    cursor.execute(query, recode)
 
-            # 5. 커넥션을 커밋해줘야 한다 => 디비에 영구적으로 반영하라는 뜻
-            connection.commit()
+                    # 5. 커넥션을 커밋해줘야 한다 => 디비에 영구적으로 반영하라는 뜻
+                    connection.commit()
 
-            # 이 포스팅의 아이디 값을 가져온다.
-            imageId = cursor.lastrowid
+                    # 이 포스팅의 아이디 값을 가져온다.
+                    imageId = cursor.lastrowid
 
-            # 6. 자원 해제
-            cursor.close()
-            connection.close()
+                    # 6. 자원 해제
+                    cursor.close()
+                    connection.close()
 
-        except mysql.connector.Error as e :
-            print(e)
-            cursor.close()
-            connection.close()
-            return {"error" : str(e)}, 503
-        
-        # 게시글 사진 id로 저장하기
-        try :
-            # 데이터 insert
-            # 1. DB에 연결
-            connection = get_connection()
+                except mysql.connector.Error as e :
+                    print(e)
+                    cursor.close()
+                    connection.close()
+                    return {"error" : str(e)}, 503
+                
+                # 게시글 사진 id로 저장하기
+                try :
+                    # 데이터 insert
+                    # 1. DB에 연결
+                    connection = get_connection()
+                    
+                    # 2. 쿼리문 만들기
+                    query = '''insert into posting_image
+                            (postingId, imageId)
+                            values
+                            (%s, %s);'''
+                            
+                    # recode 는 튜플 형태로 만든다.
+                    recode = (postingId, imageId)
+
+                    # 3. 커서를 가져온다.
+                    cursor = connection.cursor()
+
+                    # 4. 쿼리문을 커서를 이용해서 실행한다.
+                    cursor.execute(query, recode)
+
+                    # 5. 커넥션을 커밋해줘야 한다 => 디비에 영구적으로 반영하라는 뜻
+                    connection.commit()
+
+                    # 6. 자원 해제
+                    cursor.close()
+                    connection.close()
+
+                except mysql.connector.Error as e :
+                    print(e)
+                    cursor.close()
+                    connection.close()
+                    return {"error" : str(e)}, 503
             
-            # 2. 쿼리문 만들기
-            query = '''insert into posting_image
-                    (postingId, imageId)
-                    values
-                    (%s, %s);'''
-                    
-            # recode 는 튜플 형태로 만든다.
-            recode = (postingId, imageId)
-
-            # 3. 커서를 가져온다.
-            cursor = connection.cursor()
-
-            # 4. 쿼리문을 커서를 이용해서 실행한다.
-            cursor.execute(query, recode)
-
-            # 5. 커넥션을 커밋해줘야 한다 => 디비에 영구적으로 반영하라는 뜻
-            connection.commit()
-
-            # 6. 자원 해제
-            cursor.close()
-            connection.close()
-
-        except mysql.connector.Error as e :
-            print(e)
-            cursor.close()
-            connection.close()
-            return {"error" : str(e)}, 503
-        
         return {"result" : "success"}, 200
 
     # 커뮤니티 게시글 목록 리스트
     def get(self) :
-        pass
+        offset = request.args.get('offset')
+        limit = request.args.get('limit')   
+
+        if offset is None or limit is None :
+            return {'error' : '쿼리스트링 셋팅해 주세요.',
+                    'error_no' : 123}, 400
+        try :
+            # 데이터 insert
+            # 1. DB에 연결
+            connection = get_connection()
+            
+            # 2. 쿼리문 만들기
+            query = '''select p.id, p.userId, p.content, p.viewCount, i.imageUrl, p.createdAt 
+                    from posting p
+                    left join posting_image pi
+                    on p.id = pi.postingId
+                    left join images i
+                    on pi.ImageId = i.id;
+                    limit {}, {};'''.format(offset, limit)                 
+        
+            # 3. 커서를 가져온다.
+            # select를 할 때는 dictionary = True로 설정한다.
+            cursor = connection.cursor(dictionary = True)
+
+            # 4. 쿼리문을 커서를 이용해서 실행한다.
+            cursor.execute(query,)
+
+            # 5. select 문은, 아래 함수를 이용해서, 데이터를 받아온다.
+            items = cursor.fetchall()
+            
+            # 중요! 디비에서 가져온 timestamp는 
+            # 파이썬의 datetime 으로 자동 변경된다.
+            # 문제는 이 데이터를 json으로 바로 보낼 수 없으므로,
+            # 문자열로 바꿔서 다시 저장해서 보낸다.
+            i=0
+            
+            cnt = 0
+            for record in items :
+                items[i]['createdAt'] = record['createdAt'].isoformat()             
+                i = i+1
+
+
+            # 6. 자원 해제
+            cursor.close()
+            connection.close()
+
+        except mysql.connector.Error as e :
+            print(e)
+            cursor.close()
+            connection.close()
+            return {"error" : str(e)}, 503
+        
+        
+
+    
+        return {
+            "result" : "success",
+            "count" : len(items),
+            "items" : items}, 200
+
+
 
 class PostingInfoResource(Resource) :
     # 커뮤니티 게시글 수정
     @jwt_required()
-    def put(self) :
-        pass
+    def put(self, postingId) :
+        # body에서 전달된 데이터를 처리
+        # {
+        #     "title" : "점심먹자",
+        #     "date" : "2022-06-30 12:30",
+        #     "content" : "점심은 짜장면"
+        # }
+        content = request.form['content']
+
+        userId = get_jwt_identity()
+
+
+        # DB 업데이트 실행코드
+        try :
+            # 데이터 Update
+            # 1. DB에 연결
+            connection = get_connection()            
+            
+            # 2. 쿼리문 만들기
+            query = '''Update posting
+                    set content = %s
+                    where id = %s and userId = %s;'''                 
+
+            record = (content, postingId, userId)
+
+            # 3. 커서를 가져온다.
+            cursor = connection.cursor()
+
+            # 4. 쿼리문을 커서를 이용해서 실행한다.
+            cursor.execute(query, record)
+
+            # 5. 커넥션을 커밋해줘야 한다 => 디비에 영구적으로 반영하라는 뜻
+            connection.commit()
+
+
+            # 6. 자원 해제
+            cursor.close()
+            connection.close()
+        except mysql.connector.Error as e :
+            print(e)
+            cursor.close()
+            connection.close()
+            return {"error" : str(e)}, 503
+
+        # 게시글의 이미지 삭제
+        try :
+            # 데이터 Delete
+            # 1. DB에 연결
+            connection = get_connection()
+            
+            # 2. 쿼리문 만들기
+            query = '''Delete from posting_image
+                        where postingId = %s;'''                 
+            record = (postingId, )
+
+            # 3. 커서를 가져온다.
+            cursor = connection.cursor()
+
+            # 4. 쿼리문을 커서를 이용해서 실행한다.
+            cursor.execute(query, record)
+
+            # 5. 커넥션을 커밋해줘야 한다 => 디비에 영구적으로 반영하라는 뜻
+            connection.commit()
+
+            # 6. 자원 해제
+            cursor.close()
+            connection.close()
+        except mysql.connector.Error as e :
+            print(e)
+            cursor.close()
+            connection.close()
+            return {"error" : str(e)}, 503
+
+        
+        # 이미지 다시 추가
+        # photo(file), content(text)
+        photoList = ['photo1', 'photo2', 'photo3']
+        for photo in photoList :
+            if photo in request.files:
+                # 2. S3에 파일 업로드
+                # 클라이언트로부터 파일을 받아온다.
+                file = request.files[photo]
+
+                # 파일명을 우리가 변경해 준다.
+                # 파일명은, 유니크하게 만들어야 한다.
+                current_time = datetime.now()
+                new_file_name = current_time.isoformat().replace(':', '_') + ('.jpg')
+
+                # 유저가 올린 파일의 이름을 내가 만든 파일명으로 변경
+                file.filename = new_file_name
+                print(file.filename)
+                # S3에 업로드 하면 된다.
+                # AWS의 라이브러리를 사용해야 한다.
+                # 이 파이썬 라이브러리가 boto3 라이브러리다.
+                # pip install boto3
+                s3 = boto3.client('s3', 
+                            aws_access_key_id = Config.ACCESS_KEY,
+                            aws_secret_access_key = Config.SECRET_ACCESS)
+
+                try :
+                    s3.upload_fileobj(file,             # 업로드 파일
+                                    Config.S3_BUCKET,   # 버킷 url
+                                    file.filename,      # 파일명
+                                    ExtraArgs = {'ACL' : 'public-read', 'ContentType' : file.content_type})    # 권한, 타입
+
+                except Exception as e:
+                    return {'error' : str(e)}, 500       
+
+                # 사진을 DB에 저장
+                try :
+                    # 데이터 insert
+                    # 1. DB에 연결
+                    connection = get_connection()
+                    
+                    # 2. 쿼리문 만들기
+                    query = '''insert into images
+                            (userId, imageUrl)
+                            values
+                            (%s, %s);'''
+                            
+                    # recode 는 튜플 형태로 만든다.
+                    recode = (userId, file.filename)
+
+                    # 3. 커서를 가져온다.
+                    cursor = connection.cursor()
+
+                    # 4. 쿼리문을 커서를 이용해서 실행한다.
+                    cursor.execute(query, recode)
+
+                    # 5. 커넥션을 커밋해줘야 한다 => 디비에 영구적으로 반영하라는 뜻
+                    connection.commit()
+
+                    # 이 포스팅의 아이디 값을 가져온다.
+                    imageId = cursor.lastrowid
+
+                    # 6. 자원 해제
+                    cursor.close()
+                    connection.close()
+
+                except mysql.connector.Error as e :
+                    print(e)
+                    cursor.close()
+                    connection.close()
+                    return {"error" : str(e)}, 503
+                
+                # 게시글 사진 id로 저장하기
+                try :
+                    # 데이터 insert
+                    # 1. DB에 연결
+                    connection = get_connection()
+                    
+                    # 2. 쿼리문 만들기
+                    query = '''insert into posting_image
+                            (postingId, imageId)
+                            values
+                            (%s, %s);'''
+                            
+                    # recode 는 튜플 형태로 만든다.
+                    recode = (postingId, imageId)
+
+                    # 3. 커서를 가져온다.
+                    cursor = connection.cursor()
+
+                    # 4. 쿼리문을 커서를 이용해서 실행한다.
+                    cursor.execute(query, recode)
+
+                    # 5. 커넥션을 커밋해줘야 한다 => 디비에 영구적으로 반영하라는 뜻
+                    connection.commit()
+
+                    # 6. 자원 해제
+                    cursor.close()
+                    connection.close()
+
+                except mysql.connector.Error as e :
+                    print(e)
+                    cursor.close()
+                    connection.close()
+                    return {"error" : str(e)}, 503
+
+        return {'result' : 'success'}, 200
 
     # 커뮤니티 게시글 삭제
     @jwt_required()
