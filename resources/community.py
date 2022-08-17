c5448305bcc1fe7a6ee1926f5ca9b8c9e6a215f8
ghdrl858 <ghdrl858@gmail.com>
2022-08-17 12:19:39 +0900

    Update community.py

--- resources/community.py
+++ resources/community.py
@@ -230,8 +230,6 @@ class PostingListResource(Resource) :
                 # 5. select 문은, 아래 함수를 이용해서, 데이터를 받아온다.
                 images = cursor.fetchall()
                 itemImages.append(images)
-        
-
 
             # 6. 자원 해제
             cursor.close()
@@ -242,17 +240,12 @@ class PostingListResource(Resource) :
             cursor.close()
             connection.close()
             return {"error" : str(e)}, 503
-        
-        
-
     
         return {
             "result" : "success",
             "count" : len(items),
             "items" : items,
             "itemImages" : itemImages}, 200
-
-
 
 class PostingInfoResource(Resource) :
     # 커뮤니티 게시글 수정
@@ -320,9 +313,6 @@ class PostingInfoResource(Resource) :
             cursor.close()
             connection.close()
             return {"error" : str(e)}, 503
-
-
-
         
         # 이미지 다시 추가
         # photo(file), content(text)
@@ -399,11 +389,8 @@ class PostingInfoResource(Resource) :
                     # 쿼리문을 커서를 이용해서 실행한다.
                     cursor.execute(query, recode)
 
-
                     # 커넥션을 커밋해줘야 한다 => 디비에 영구적으로 반영하라는 뜻
                     connection.commit()
-
-                    
 
                     # 6. 자원 해제
                     cursor.close()
@@ -414,7 +401,6 @@ class PostingInfoResource(Resource) :
                     cursor.close()
                     connection.close()
                     return {"error" : str(e)}, 503
-                
 
         return {'result' : 'success'}, 200
 
