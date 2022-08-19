40b9ebe817eb4435baeb5636244118296eeb82f4
minui2167 <minui2167@gmail.com>
2022-08-19 13:06:28 +0900

    거래 신청, 취소, 완료 기능 추가

--- resources/goods.py
+++ resources/goods.py
@@ -904,9 +904,189 @@ class GoodsDealResource(Resource) :
         return {"result" : "success"}, 200
 
     # 거래 취소하기
-    def put(self, goodsId) :
-        pass
+    @jwt_required()
+    def delete(self, goodsId) :
+        # 내가 거래중인 구매자가 맞는지 확인
+        # 상품이 거래중인 상태가 맞는지 확인 (status == 1)
+        # 상품의 상태를 0으로 변경
+        userId = get_jwt_identity()
+        
+        try :
+            # 1. DB에 연결
+            connection = get_connection()   
+
+            # 게시글 가져오기         
+            query = '''select * from buy
+                    where buyerId = %s and goodsId = %s'''
+
+            record = (userId, goodsId)
+            # 3. 커서를 가져온다.
+            # select를 할 때는 dictionary = True로 설정한다.
+            cursor = connection.cursor(dictionary = True)
+
+            # 4. 쿼리문을 커서를 이용해서 실행한다.
+            cursor.execute(query, record)
+
+            # 5. select 문은, 아래 함수를 이용해서, 데이터를 받아온다.
+            items = cursor.fetchall()
+            
+            # 판매자가 아닌지 확인
+            if not items :
+                return {"error" : "구매자가 아닙니다."}, 200
+
+            # 게시글 가져오기         
+            query = '''select * from goods
+                    where id = %s'''
+
+            record = (goodsId, )
+            # 3. 커서를 가져온다.
+            # select를 할 때는 dictionary = True로 설정한다.
+            cursor = connection.cursor(dictionary = True)
+
+            # 4. 쿼리문을 커서를 이용해서 실행한다.
+            cursor.execute(query, record)
+
+            # 5. select 문은, 아래 함수를 이용해서, 데이터를 받아온다.
+            items = cursor.fetchall()
+
+            # 거래중 상태 확인
+            if items[0]['status'] != 1 :
+                return {"error" : "거래 중인 상태가 아닙니다."}, 200
+
+
+            # 거래 신청을 하면 buy 테이블에 상품, 구매자 삭제
+            # 2. 쿼리문 만들기
+            query = '''Delete from buy
+                        where buyerId = %s and goodsId = %s;'''
+                    
+            # recode 는 튜플 형태로 만든다.
+            recode = (userId, goodsId)
+
+            # 3. 커서를 가져온다.
+            cursor = connection.cursor()
+
+            # 4. 쿼리문을 커서를 이용해서 실행한다.
+            cursor.execute(query, recode)
+
+
+            # 상품의 상태를 1 (거래 중)로 변경
+            query = '''Update goods
+                    set status = 0
+                    where id = %s;'''
+                    
+            # recode 는 튜플 형태로 만든다.
+            recode = (goodsId, )
+
+            # 3. 커서를 가져온다.
+            cursor = connection.cursor()
+
+            # 4. 쿼리문을 커서를 이용해서 실행한다.
+            cursor.execute(query, recode)              
+
+            # 5. 커넥션을 커밋해줘야 한다 => 디비에 영구적으로 반영하라는 뜻
+            connection.commit()            
+            # 6. 자원 해제
+            cursor.close()
+            connection.close()
+
+        except mysql.connector.Error as e :
+            print(e)
+            cursor.close()
+            connection.close()
+            return {"error" : str(e)}, 503
+
+        return {"result" : "success"}, 200
 
     # 거래 완료하기
-    def delete(self, goodsId) :
-        pass
+    @jwt_required()
+    def put(self, goodsId) :
+        # 내가 거래중인 구매자가 맞는지 확인
+        # 상품이 거래중인 상태가 맞는지 확인 (status == 1)
+        # 상품의 상태를 2으로 변경
+        userId = get_jwt_identity()
+        
+        try :
+            # 1. DB에 연결
+            connection = get_connection()   
+
+            # 게시글 가져오기         
+            query = '''select * from buy
+                    where buyerId = %s and goodsId = %s'''
+
+            record = (userId, goodsId)
+            # 3. 커서를 가져온다.
+            # select를 할 때는 dictionary = True로 설정한다.
+            cursor = connection.cursor(dictionary = True)
+
+            # 4. 쿼리문을 커서를 이용해서 실행한다.
+            cursor.execute(query, record)
+
+            # 5. select 문은, 아래 함수를 이용해서, 데이터를 받아온다.
+            items = cursor.fetchall()
+            
+            # 판매자가 아닌지 확인
+            if not items :
+                return {"error" : "구매자가 아닙니다."}, 200
+
+            # 게시글 가져오기         
+            query = '''select * from goods
+                    where id = %s'''
+
+            record = (goodsId, )
+            # 3. 커서를 가져온다.
+            # select를 할 때는 dictionary = True로 설정한다.
+            cursor = connection.cursor(dictionary = True)
+
+            # 4. 쿼리문을 커서를 이용해서 실행한다.
+            cursor.execute(query, record)
+
+            # 5. select 문은, 아래 함수를 이용해서, 데이터를 받아온다.
+            items = cursor.fetchall()
+
+            # 거래중 상태 확인
+            if items[0]['status'] != 1 :
+                return {"error" : "거래 중인 상태가 아닙니다."}, 200
+
+
+            # 거래 신청을 하면 buy 테이블에 상품, 구매자 삭제
+            # 2. 쿼리문 만들기
+            query = '''Delete from buy
+                        where buyerId = %s and goodsId = %s;'''
+                    
+            # recode 는 튜플 형태로 만든다.
+            recode = (userId, goodsId)
+
+            # 3. 커서를 가져온다.
+            cursor = connection.cursor()
+
+            # 4. 쿼리문을 커서를 이용해서 실행한다.
+            cursor.execute(query, recode)
+
+
+            # 상품의 상태를 1 (거래 중)로 변경
+            query = '''Update goods
+                    set status = 2
+                    where id = %s;'''
+                    
+            # recode 는 튜플 형태로 만든다.
+            recode = (goodsId, )
+
+            # 3. 커서를 가져온다.
+            cursor = connection.cursor()
+
+            # 4. 쿼리문을 커서를 이용해서 실행한다.
+            cursor.execute(query, recode)              
+
+            # 5. 커넥션을 커밋해줘야 한다 => 디비에 영구적으로 반영하라는 뜻
+            connection.commit()            
+            # 6. 자원 해제
+            cursor.close()
+            connection.close()
+
+        except mysql.connector.Error as e :
+            print(e)
+            cursor.close()
+            connection.close()
+            return {"error" : str(e)}, 503
+
+        return {"result" : "success"}, 200
