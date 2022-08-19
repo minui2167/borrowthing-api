9128a69b5ae2f63111fac0b11ad7d00005907e40
minui2167 <minui2167@gmail.com>
2022-08-19 12:19:15 +0900

    Update goods.py

--- resources/goods.py
+++ resources/goods.py
@@ -820,3 +820,94 @@ class GoodsRecommendResource(Resource) :
      # 추천하는 빌려주기 글 가져오기
      def get(self) :
         pass
+
+class GoodsDealResource(Resource) :
+    @jwt_required()
+    # 거래 신청하기
+    def post(self) :
+        # 상품의 거래상태가 0 (거래 대기)일 때, 거래신청이 가능
+        # 거래 신청을 하면 buy 테이블에 상품, 구매자 추가
+        # 상품의 상태를 1 (거래 중)로 변경
+        userId = get_jwt_identity()
+        data = request.get_json()
+        print(data['goodsId'])
+        try :
+            # 1. DB에 연결
+            connection = get_connection()   
+
+            # 게시글 가져오기         
+            query = '''select * from goods
+                    where id = %s'''
+
+            record = (data['goodsId'], )
+            # 3. 커서를 가져온다.
+            # select를 할 때는 dictionary = True로 설정한다.
+            cursor = connection.cursor(dictionary = True)
+
+            # 4. 쿼리문을 커서를 이용해서 실행한다.
+            cursor.execute(query, record)
+
+            # 5. select 문은, 아래 함수를 이용해서, 데이터를 받아온다.
+            items = cursor.fetchall()
+            
+            # 판매자가 아닌지 확인
+            if items[0]['sellerId'] == userId :
+                return {"error" : "본인 글입니다."}, 200
+
+            # 거래대기 상태 확인
+            if items[0]['status'] != 0 :
+                return {"error" : "거래 대기 상태가 아닙니다."}, 200
+
+
+            # 거래 신청을 하면 buy 테이블에 상품, 구매자 추가
+            # 2. 쿼리문 만들기
+            query = '''insert into buy
+                    (buyerId, goodsId)
+                    values
+                    (%s, %s);'''
+                    
+            # recode 는 튜플 형태로 만든다.
+            recode = (userId, data['goodsId'])
+
+            # 3. 커서를 가져온다.
+            cursor = connection.cursor()
+
+            # 4. 쿼리문을 커서를 이용해서 실행한다.
+            cursor.execute(query, recode)
+
+
+            # 상품의 상태를 1 (거래 중)로 변경
+            query = '''Update goods
+                    set status = 1
+                    where id = %s;'''
+                    
+            # recode 는 튜플 형태로 만든다.
+            recode = (data['goodsId'], )
+
+            # 3. 커서를 가져온다.
+            cursor = connection.cursor()
+
+            # 4. 쿼리문을 커서를 이용해서 실행한다.
+            cursor.execute(query, recode)              
+
+            # 5. 커넥션을 커밋해줘야 한다 => 디비에 영구적으로 반영하라는 뜻
+            connection.commit()            
+            # 6. 자원 해제
+            cursor.close()
+            connection.close()
+
+        except mysql.connector.Error as e :
+            print(e)
+            cursor.close()
+            connection.close()
+            return {"error" : str(e)}, 503
+
+        return {"result" : "success"}, 200
+
+    # 거래 취소하기
+    def post(self) :
+        pass
+
+    # 거래 완료하기
+    def delete(self) :
+        pass
