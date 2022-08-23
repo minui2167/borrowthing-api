fa72f09c97039ccc6a7cd07dc352e964ebeabe42
minui2167 <minui2167@gmail.com>
2022-08-23 16:50:04 +0900

    order by createdAt

--- resources/goods.py
+++ resources/goods.py
@@ -46,7 +46,7 @@ class GoodsListResource(Resource) :
                         on g.id = gi.goodsId
                         group by g.id) imgCount
                         where g.id = wishCount.id and g.id = commentCount.id and g.id = imgCount.id
-                        order by g.updatedAt desc
+                        order by g.createdAt desc
                         limit {}, {};'''.format(offset, limit) 
 
             # 3. 커서를 가져온다.
@@ -450,7 +450,7 @@ class GoodsListInAreaResource(Resource) :
                                             on g.id = wl.goodsId and wl.userId = %s
                                             group by g.id) isWish                     
                     where g.id = wishCount.id and g.id = commentCount.id and g.id = imgCount.id and g.id = isWish.id
-                    order by g.updatedAt desc
+                    order by g.createdAt desc
                     limit {}, {};'''.format(offset, limit) 
 
             record = (userId, userId, userId)
